80ff41d8a8c7f6ddc0c5f934dcb388bd261f3b71
cuiwei0510 <cuiwei0510@126.com>
2019-11-21 15:04:43 +0800

    Initial commit

---
+++ src/com/MyClass.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @author cuiwei
+ * @create 2019-11-21 下午 14:58
+ */
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
